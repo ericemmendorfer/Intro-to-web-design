944f29ab60854bf52243e1855fa80c081669f677
Eric Emmendorfer <emmendorfer.eric@gmail.com>
2025-02-03 18:14:43 -0500

    Add files via upload

---
+++ final project/script.js
@@ -0,0 +1,43 @@
+function contact(){
+var w=dt.getDay();
+
+if (w==0){
+	  window.alert("Happy Sunday, I am unfortunately not available to work today.");
+
+
+}
+else if (w==1){
+ window.alert("Happy Monday,I am unfortunately not available to work today");
+
+
+}
+else if (w==2){
+ window.alert("Happy Tuesday, I am avaibale to work today");
+
+
+}
+else if (w==3){
+ window.alert("Happy Wednesday, I am unfortunately not available to work today");
+
+
+}
+else if (w==4){
+ window.alert("Happy Thursday, I am avaibale to work today");
+
+
+}
+else if (w==5){
+ window.alert("Happy Friday, I am avaibale to work today");
+
+
+}
+else{
+ window.alert("Happy Saturday, I am avaibale to work today");
+}
+}
+
+
+
+function (){
+
+}
